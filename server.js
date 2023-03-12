3c4d0d304652c874b2659bdb7c2baabbd5158925
Pranav Deshmukh <pranavmdeshmukh047@gmail.com>
2023-03-12 10:06:45 +0530

    changes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(URI, {
   .catch(err => console.log(err));
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log('Server is running on port', PORT)
 })
